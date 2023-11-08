ae0141db23d814565a05e0df2984b38ca55f5666
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-09 01:50:05 +0300

    create a class to manage the API authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,13 +11,11 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """method that returns False path and excluded paths"""
         return False
-    
 
     def authorization_header(self, request=None) -> str:
         """method for authorization header"""
         if request is None:
             return None
-    
 
     def current_user(self, request=None) -> TypeVar('User'):
         """method that returns none"""
