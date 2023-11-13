7c35655e0dc78489f070f00b324967cca38635bc
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-14 01:17:58 +0300

    Method that returns users instance based on email and password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -63,6 +63,7 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
         users = User.search({"email": user_email})
+        print(f"Search results: {users}")
         if not users or users == []:
             return None
         user = users[0]
