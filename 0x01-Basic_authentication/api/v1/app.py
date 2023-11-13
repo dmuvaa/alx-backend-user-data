4cebac10cdfbe0da53dbfbf8cacb00c809b4bcac
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-13 22:47:02 +0300

    validate all requests to secure the API

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -30,10 +30,10 @@ def before_request_handler():
     """Add a before request handler"""
     if auth is None:
         return
-    
+
     paths = ['/api/v1/status/',
              '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    
+
     if auth.require_auth(request.path, paths):
         if auth.authorization_header(request) is None:
             abort(401, description="Unauthorized")
