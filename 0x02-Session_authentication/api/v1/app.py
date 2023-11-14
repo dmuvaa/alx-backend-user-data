54175223b938c236dc81a8de1ebb9f8470da77af
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-14 20:06:21 +0300

    Empty Session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -5,6 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -24,6 +25,8 @@ if auth_type == 'auth':
 elif auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
 
 
 @app.before_request
@@ -36,12 +39,11 @@ def before_request_handler():
              '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
     if auth.require_auth(request.path, paths):
+        request.current_user = auth.current_user(request)
         if auth.authorization_header(request) is None:
             abort(401, description="Unauthorized")
-        request.current_user = auth.current_user(request)
         if auth.current_user(request) is None:
             abort(403, description="Forbidden")
-
 
 @app.errorhandler(404)
 def not_found(error) -> str:
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Import a Module"""
+
+from api.v1.auth.auth import Auth
+
+"""Create a Class"""
+
+
+class SessionAuth(Auth):
+    """Empty Class"""
+    pass
