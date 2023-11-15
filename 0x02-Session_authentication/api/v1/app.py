39d2ad6cd0fbbb2b9e0b5962a081cc9dd2d2a1f1
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-16 01:16:44 +0300

    Before Request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,20 +32,26 @@ elif auth_type == 'session_auth':
 
 @app.before_request
 def before_request_handler():
-    """Add a before request handler"""
+    """ Add a before request handler """
     if auth is None:
         return
 
-    paths = ['/api/v1/status', '/api/v1/status/'
-             '/api/v1/unauthorized/', '/api/v1/forbidden/'
-             '/api/v1/auth_session/login/']
+    exempt_paths = [
+        '/api/v1/status',
+        '/api/v1/unauthorized',
+        '/api/v1/forbidden',
+        '/api/v1/auth_session/login'
+    ]
 
-    if auth.require_auth(request.path, paths):
-        request.current_user = auth.current_user(request)
+    if auth.require_auth(request.path, exempt_paths):
+        current_user = auth.current_user(request)
+        request.current_user = current_user
+
         if auth.authorization_header(
                 request) is None and auth.session_cookie(request) is None:
             abort(401, description="Unauthorized")
-        if auth.current_user(request) is None:
+
+        if current_user is None:
             abort(403, description="Forbidden")
 
 
