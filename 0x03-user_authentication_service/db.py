c19105a1fc5c710201c9d5938ff7308559c4e8da
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-17 19:45:20 +0300

    New Method that Adds User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,11 +31,14 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """Method that add user to the database"""
-        new_user = User(email=email, hashed_password=hashed_password)
-        session = self._session
-        session.add(new_user)
-        session.commit()
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self.session.rollback()
+            new_user = None
         return new_user
