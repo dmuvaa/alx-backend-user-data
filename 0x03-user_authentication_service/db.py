70741fd4f3d2f8b59c071c98ee1175fa395f3df1
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-17 19:51:58 +0300

    New Method that Adds User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -18,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -39,6 +39,6 @@ class DB:
             self._session.add(new_user)
             self._session.commit()
         except Exception:
-            self.session.rollback()
+            self._session.rollback()
             new_user = None
         return new_user
