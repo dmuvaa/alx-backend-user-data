d70dbe446c27b09b76d2bb538103494df37e38cf
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-17 18:28:46 +0300

    Mapping Declaration in SQLAlchemy

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+"""import sqlalchemy modules"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -13,13 +15,11 @@ class User(Base):
     """Class that inherits from Base"""
     __tablename__ = 'users'
 
-
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
 
     def __repr__(self):
         """Main Self"""
