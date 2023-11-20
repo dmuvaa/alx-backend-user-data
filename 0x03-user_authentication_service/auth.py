67baade5524ff59d5da41496597e7f73226aa76a
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-20 17:54:25 +0300

    Method that Returns an Hashed Password from String

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Import Modules"""
+
+import bcrypt
+
+"""Create a Method"""
+
+
+def _hash_password(password) -> str:
+    """Method that takes password string arguments and returns bytes"""
+    bytes = password.encode('utf-8')
+
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(bytes, salt)
+    return hashed_password
