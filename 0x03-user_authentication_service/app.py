8f48d102f95cd6be7074f0c0edeb675e33f7556a
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-20 19:05:06 +0300

    CrEnd Point to Register New User

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,15 +2,29 @@
 
 """Import Modules"""
 
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def basic_app():
     """Method that returns a JSON payload of the form"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """end-point to register a user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        new_user = AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
